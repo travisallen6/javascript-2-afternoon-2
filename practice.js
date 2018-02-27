13e53df5c7c61cabd53ae4af88b7baee084fe021
Travis Allen <travisallen6@gmail.com>
2018-02-27 15:29:54 -0700

    Added answers through 9

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for(i=0; i < family.length; i++){
+    alert(family[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(i=letters.length-1; i>=0; i--){
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +90,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums){
+  var newArray = [];
+  for(i=0; i<nums.length; i++){
+    if(nums[i] %2 === 0){
+      newArray.push(nums[i]);
+    }
+  }
+  return newArray;
+}
 
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
@@ -95,6 +115,20 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+function divider(numbersArray){
+  var evenArray = [];
+  var oddArray = [];
+  for(i=0; i<numbersArray.length; i++){
+    if(numbersArray[i] %2 === 0){
+      evenArray.push(numbersArray[i]);
+    }
+    if(numbersArray[i] %2 === 1){
+      oddArray.push(numbersArray[i]);
+    }
+  }
+  let finalArray = [evenArray,oddArray];
+  return finalArray;
+}
 
 
 
@@ -116,7 +150,15 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
-
+function finder(array){
+  let random = getRandomArbitrary();
+  for(i=0; i<array.length; i++){
+    if(array[i]===random){
+      return true;
+    }
+  }
+  return false;
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -143,7 +185,7 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
-
+function removeItem(myGroceryList,item)
 
 
 ////////// PROBLEM 9 //////////
